df1670b5d14b1c5f5911dd96ee572b494ba94fa5
郑小松 <zhengxiaosong@linggan.com>
2016-04-19 20:56:05 +0800

    add query module

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.jsbridge = require('./jsbridge');
 exports.packages = require('./packages');
 exports.common = require('./common');
+exports.location = require('./location');
+exports.query = require('./query');
