11191f1ed2788a4c787fa63e040354a3d6dbd52e
郑小松 <zhengxiaosong@linggan.com>
2016-04-19 22:01:31 +0800

    重构公共部分代码

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// 比较久的桥
 exports.jsbridge = require('./jsbridge');
 exports.packages = require('./packages');
 exports.common = require('./common');
+
+exports.base64 = require('./base64');
+exports.json = require('./json');
+// 分离出来的桥部分
+exports.bridge = require('./bridge');
+
+// 重写掉 window.location，添加query字段
 exports.location = require('./location');
 exports.query = require('./query');
